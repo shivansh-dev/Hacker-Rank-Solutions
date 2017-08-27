fac2eb42deae0b6622681841ae390ccf90ba9abf
Shivansh Tyagi <30756165+stshivansh9@users.noreply.github.com>
2017-08-27 12:12:53 +0530

    organised

--- String4.java
+++ String4.java
@@ -1,41 +1,23 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class String4 {
-  
-	public static String getSmallestAndLargest(String s, int k) {
-        String smallest = "";
-        String largest = "";
-        int lenOfS=s.length()-k+1;
-        String[] subs = new String[lenOfS];
-        
-        for (int i=0;i<lenOfS;i++) {
-        	subs[i]= s.substring(i,i+k);
-        	}
-        Arrays.sort(subs);
+public class String5 {
 
-        smallest=subs[0];
-        largest=subs[subs.length-1];
-               
-        
-        
-        // Complete the function
-        // 'smallest' must be the lexicographically smallest substring of length 'k'
-        // 'largest' must be the lexicographically largest substring of length 'k'
-        
-        return smallest + "\n" + largest;
-    } 
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String s = scan.next(); 
-        int k = scan.nextInt();
-        
-       
-   
-        scan.close();
-        
-      
-        System.out.println(getSmallestAndLargest(s,k));
-    }
+	    public static void main(String[] args) {
+	        
+	    	Scanner sc=new Scanner(System.in);
+	        String str = sc.next();
+	        StringBuffer A= new StringBuffer(str);
+	        StringBuffer B= new StringBuffer(str);
+	        B.reverse();
+	        if(A.toString().compareTo(B.toString())==0)
+	        {
+	        	System.out.println("Yes");
+	        }
+	        else
+	        { System.out.println("No");
+	        }
+	        
+	        
+	    }
+	
 }
