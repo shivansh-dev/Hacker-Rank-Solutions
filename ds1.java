fbf83260d5c23a94c600cc485ba4a743ff86252d
Shivansh Tyagi <30756165+stshivansh9@users.noreply.github.com>
2017-09-02 16:12:07 +0530

    a

---
+++ ds1.java
@@ -0,0 +1,17 @@
+
+public class ds1 {
+
+	public static void main(String[] args) {
+	   
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        // Declare array a here
+        int[] a = new int[n];
+        for(int i = 0 ; i < n; i++){
+            a[i] = scan.nextInt();
+            // Fill array a here
+        }
+
+	}
+
+}
